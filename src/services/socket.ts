880498f1f9889d67c980b4a1e6fb166c3c8aeb9f
Nattaporn Detdee <nattaporn.d@hoteliers.guru>
2026-01-24 23:13:19 +0700

    fix: use url VITE_SOCKET_URL from .env

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,15 @@
 import { io } from 'socket.io-client';
 
-// Point to NestJS Backend URL
-export const socket = io('http://localhost:3000', {
-  transports: ['websocket']
+// Get the Socket URL from Environment Variables
+const URL = import.meta.env.VITE_SOCKET_URL;
+
+// Check if VITE_SOCKET_URL is set
+if (!URL) {
+  throw new Error('VITE_SOCKET_URL is not defined in .env file');
+}
+
+export const socket = io(URL, {
+  transports: ['websocket'],
+  // It's recommended to disable auto-connection and connect manually when needed
+  // autoConnect: false,
 });
